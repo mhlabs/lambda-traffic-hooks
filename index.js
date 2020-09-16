509a5811f417477d3c2bcb06c55cb1eac7d3b565
Mattias Liljenzin <mattias.liljenzin@gmail.com>
2020-09-16 09:20:15 +0200

    fix: remove test-tests

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const hooks = require('./hooks');
 
-hooks.add('/health', 200);
-hooks.add('/ping', 200);
+// Example:
+// hooks.add('/health', 200);
+// hooks.add('/ping', 200);
 
 module.exports = hooks;
